f5724d3d2a771c626c1e79475df0f818ceb5164d
Ridwan226 <4515210063@univpancasila.ac.id>
2021-08-31 22:04:51 +0700

    CRUD Category

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,7 +1,65 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("admin/category/view_category");
+      const category = await Category.find();
+
+      res.render("admin/category/view_category", {
+        category,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/category/create");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  actionCreate: async (req, res) => {
+    try {
+      const {name} = req.body;
+
+      let category = await Category({name: name});
+      await category.save();
+      res.redirect("/category");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  viewEdit: async (req, res) => {
+    try {
+      const {id} = req.params;
+      const category = await Category.findOne({_id: id});
+      res.render("admin/category/edit", {category});
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  acctionUpdate: async (req, res) => {
+    try {
+      const {id} = req.params;
+      const {name} = req.body;
+      const category = await Category.findOneAndUpdate({_id: id}, {name});
+      // console.log(id);
+      res.redirect("/category");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  acctionDel: async (req, res) => {
+    try {
+      const {id} = req.params;
+      const category = await Category.findOneAndRemove({_id: id});
+      res.redirect("/category");
     } catch (err) {
       console.log(err);
     }
--- app/category/router.js
+++ app/category/router.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const { index } = require('./controller');
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  viewEdit,
+  acctionUpdate,
+  acctionDel,
+} = require("./controller");
 
 /* GET home page. */
-router.get('/', index);
+router.get("/", index);
+router.get("/create", viewCreate);
+router.post("/create", actionCreate);
+router.get("/edit/:id", viewEdit);
+router.put("/update/:id", acctionUpdate);
+router.delete("/del/:id", acctionDel);
 
 module.exports = router;
