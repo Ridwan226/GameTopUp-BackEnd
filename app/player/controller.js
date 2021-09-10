7395b63e3ffb1164d9c833b11a5abbc89c7085a9
Ridwan226 <4515210063@univpancasila.ac.id>
2021-09-11 02:38:13 +0700

    History user

--- app/player/controller.js
+++ app/player/controller.js
@@ -118,4 +118,59 @@ module.exports = {
         .json({message: err.message || `Terjadi kesalahan pada server`});
     }
   },
+
+  history: async (req, res) => {
+    try {
+      const {status = ""} = req.query;
+
+      let criteria = {};
+
+      if (status.length) {
+        criteria = {
+          ...criteria,
+          status: {$regex: `${status}`, $options: "i"},
+        };
+      }
+
+      if (req.player._id) {
+        criteria = {
+          ...criteria,
+          player: req.player._id,
+        };
+      }
+
+      const history = await Transaction.find(criteria);
+
+      let total = await Transaction.aggregate([
+        {$match: criteria},
+        {$group: {_id: null, value: {$sum: "$value"}}},
+      ]);
+
+      res
+        .status(200)
+        .json({data: history, total: total.length ? total[0].value : 0});
+    } catch (err) {
+      res
+        .status(500)
+        .json({message: err.message || `Terjadi kesalahan pada server`});
+    }
+  },
+
+  historyDetail: async (req, res) => {
+    try {
+      const {id} = req.params;
+
+      const history = await Transaction.findOne({_id: id});
+
+      if (!history) {
+        return res.status(404).json({message: `history Game Tidak di Temukan`});
+      }
+
+      res.status(200).json({data: history});
+    } catch (err) {
+      res
+        .status(500)
+        .json({message: err.message || `Terjadi kesalahan pada server`});
+    }
+  },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -2,12 +2,21 @@ var express = require("express");
 var router = express.Router();
 const {isLoginPlayer} = require("../middleware/auth");
 
-const {landingPage, detailPage, category, checkout} = require("./controller");
+const {
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+  historyDetail,
+} = require("./controller");
 
 /* GET home page. */
 router.get("/landingpage", landingPage);
 router.get("/category", category);
 router.get("/:id/detail", detailPage);
 router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
+router.get("/history", isLoginPlayer, history);
 
 module.exports = router;
